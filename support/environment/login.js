1b632f6cbad3ff748f07c7e088b804c5907c98c9
BryanM3rlos <58125637+BryanM3rlos@users.noreply.github.com>
2020-01-25 21:10:53 -0600

    second commit

---
+++ support/environment/login.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "invalid_credencials": {
+    "email": "123@testInvalid.com",
+    "password": "123",
+    "assertion_title": "Invalid user"
+  },
+  "valid_credentials": {
+    "email": "bryan.merlosd@gmail.com",
+    "password": "test123@",
+    "assertion_title": "Welcome to",
+    "assertion_page": "My profile"
+  }
+}
