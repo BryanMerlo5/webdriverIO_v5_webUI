ef4202a707676cf0ac9f51fc5e14657b22617c9d
BryanMerlo5 <bryan.merlosd@gmail.com>
2020-03-09 19:46:06 -0600

    new changes about browser

--- src/features/demo.feature
+++ src/features/demo.feature
@@ -1,8 +1,4 @@
-  
-Feature: Test if the url is a certain value
-    As a developer
-    I want to be able to test if the url is a certain value
+Feature: Update xBand status to Replacement
 
-   Scenario: The url should not be http://www.google.com/
-        Given I open the site "https://www.amazon.com.mx/"
-        Then I wait for element visible
+  Scenario: Update xBand status to Replacement
+    Given I open a website 'elem.txt.info'
---
+++ src/maps/mapping_One.js
@@ -0,0 +1,34 @@
+module.exports = {
+    btn: {
+        search: '#Tg7LZd'
+    },
+    txt: {
+        info: '//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input'
+    },
+    img: {
+        doodle: '//*[@id="hplogo"]/div[2]'
+    },
+    tbl:{
+
+    },
+    lnk: {
+        firstResult: '//*[@id="rso"]/div[1]/div/div/link'
+    },
+    getCall: function() {     
+    },
+    /**
+     * Getting the Response Timing
+     */
+    resTime: function() {
+    },
+    /**
+     * Getting the Status Code
+     */
+    staCode: function() {
+    },
+    /**
+     * Getting the Content of the API
+     */
+    contApi: function() {
+    }
+  };
